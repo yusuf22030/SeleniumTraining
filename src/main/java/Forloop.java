6a7c8043e2dc8dd835c5584f64b1b988b31125dc
Ismail Osmanjan <ismail.osmanjan@verizonmedia.com>
2020-07-12 15:50:01 -0400

    iiiiii

--- src/main/java/Forloop.java
+++ src/main/java/Forloop.java
@@ -1,7 +1,9 @@
 public class Forloop {
   public static void main(String[] args) {
     for (int i = 0; i <= 5; i++) {
-      System.out.println("yusuf"); //
+
+
+      System.out.println("yusuf " + i); //
     }
   }
 }
